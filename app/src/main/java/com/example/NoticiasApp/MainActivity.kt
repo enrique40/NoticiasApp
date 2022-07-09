062ba39c7aa0f46def98ec495dc0adf4008c9a73
enrique <daniellara5a2015@gmail.com>
2022-07-09 14:39:05 -0400

    agregando api_key

---
+++ app/src/main/java/com/example/NoticiasApp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.NoticiasApp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.noticiasapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
